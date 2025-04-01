cbd1c15dedfeaa20570b3ff9f002d2e82754b3b2
nichole06 <arajetjohn@gmail.com>
2025-04-01 22:31:43 +0800

    Added .gitignore file (added .vscode, bin, and obj in it. Also deleted the .vscode, bin and obj in the remote repo.

--- Database/MockDatabase.cs
+++ Database/MockDatabase.cs
@@ -6,8 +6,8 @@ public class MockDatabase : IAccountRepository
 {
    private List<Account> accounts = new List<Account>
    {
-      new Account("123456789", "12345", "Nichole John Tejara", 0),
-      new Account("12345678", "1234321", "Mary Stephany Gweneth Pizana", 0),
+      new Account("123456789", "12345", "Sample Name", 0), // change this if you want to.
+      new Account("12345678", "1234", "Sample Name 2", 0),
    };
 
    public List<Account> GetAllAccounts()
